9aa6b6a5da5c4b2b555fab003f50b73019441305
La-Kiki <yakira2797@hotmail.com>
2020-02-22 11:35:45 +0000

    CSS style is missing circle arrangement

---
+++ public/js/common/userProfiles.js
@@ -0,0 +1,45 @@
+
+let userProfiles = [
+
+    {
+	"name": "Miguel",
+	"age": "",
+	"photo": "img/common/m1.png",
+	"gender": "",
+	"searching": "",
+	"hobbies": "", 
+	"description": ""
+    },
+
+    {
+	"name": "Maiko",
+	"age": "",
+	"photo": "img/common/w2.png",
+	"gender": "",
+	"searching": "",
+	"hobbies": "", 
+	"description": ""
+    },
+
+    {
+	"name": "Deepika",
+	"age": "",
+	"photo": "img/common/w5.png",
+	"gender": "",
+	"searching": "",
+	"hobbies": "", 
+	"description": ""
+    },
+    
+    {
+	"name": "Jason",
+	"age": "",
+	"photo": "img/common/m4.png",
+	"gender": "",
+	"searching": "",
+	"hobbies": "", 
+	"description": ""
+    },
+
+
+]
---
+++ public/js/organizer/vue_script.js
@@ -0,0 +1,15 @@
+
+const vm = new Vue({
+
+    el: '#dashboardContent',
+    data:{
+
+	profiles: userProfiles,  //Imported from JSON
+
+	computed: {  //May be needed for grid arrangement
+	    
+	}
+
+    },
+});
+
