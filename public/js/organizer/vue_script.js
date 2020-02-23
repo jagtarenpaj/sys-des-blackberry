7e2819920a0c8ae3f9d8f49c5f174d3de07cdd16
La-Kiki <yakira2797@hotmail.com>
2020-02-23 19:22:08 +0000

    Profiles are paired but not spaced

--- public/js/organizer/vue_script.js
+++ public/js/organizer/vue_script.js
@@ -5,11 +5,38 @@ const vm = new Vue({
     data:{
 
 	profiles: userProfiles,  //Imported from JSON
-
-	computed: {  //May be needed for grid arrangement
-	    
-	}
-
+	degreeSpace: [],
     },
+
+    created() {
+	//Arranges grid spacing dynamically with number of profiles
+	//Value must be used at least once to be defined
+	for(let i= 0; i < this.profiles.length; ++i ){
+
+	    this.degreeSpace[i] = (i * 2) * (360/this.profiles.length);   /*  *2 to match up pairs?*/
+	}
+    },
+
+	
+
+    methods: {
+
+	spaceProfile: function(index, person){
+	    
+	    //Wait until DOM element is loaded to execute
+	    this.$nextTick(
+		function(){
+		    let profile = document.getElementById(person.name);
+
+		    profile.style.transform =
+			'rotate(' + this.degreeSpace[index] + 'deg) translate(350%) rotate(' + (-this.degreeSpace[index]) + 'deg)';    
+		    if(profile.classList.contains('profile00')){
+			
+		    }
+		}
+	    );	    
+	},
+    }
+    
 });
 
